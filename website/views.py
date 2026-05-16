7559607ae9b1ca14ef13d5a77cc3da940a30bb43
CyeVrl <cyrenvrl@gmail.com>
2026-05-16 20:32:57 +0800

    For db nung current and max capacity

--- website/views.py
+++ website/views.py
@@ -1,16 +1,56 @@
 from flask import Blueprint, render_template
+from .db import get_db_connection
 
 views = Blueprint('views',__name__)
 
 # students/profs to log in
+#Get information from db current and capacity to show current and max capacity (seatVacancy)
 @views.route('/')
 def CapacityTrackingUserLogin():
-    return render_template("CapacityTrackingUserLogin.html")
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
 
-#student/prof confirms seat
+    cursor.execute("""
+        SELECT area, current, capacity, floor
+        FROM vacancy_table
+    """)
+
+    rooms = cursor.fetchall()
+
+    cursor.close()
+    db.close()
+    return render_template("CapacityTrackingUserLogin.html", rooms=rooms)
+
+#Get information from db current and capacity to show current and max capacity (seatSelection)
 @views.route('/CapacityTrackingConfirmSeat')
 def CapacityTrackingConfirmSeat():
-    return render_template("CapacityTrackingConfirmSeat.html")
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
 
+    cursor.execute("""
+        SELECT area, current, capacity, floor
+        FROM vacancy_table
+    """)
 
+    rooms = cursor.fetchall()
 
+    cursor.close()
+    db.close()
+    return render_template("CapacityTrackingConfirmSeat.html", rooms=rooms)
+
+#Get information from db current and capacity to show current and max capacity (Ril capacityTracking)
+@views.route('/CapacityTrackingAdminSide')
+def CapacityTrackingAdminSide():
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT area, current, capacity, floor
+        FROM vacancy_table
+    """)
+
+    rooms = cursor.fetchall()
+
+    cursor.close()
+    db.close()
+    return render_template("CapacityTrackingAdminSide.html", rooms=rooms)
